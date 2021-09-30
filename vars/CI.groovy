2123dcb8141c248def680da320d684d5b1a03414
sunkaravineeth <63944740+sunkaravineeth@users.noreply.github.com>
2021-09-30 12:51:11 +0530

    Update CI.groovy

--- vars/CI.groovy
+++ vars/CI.groovy
@@ -1,1 +1,18 @@
-sample
+import com.GitUtil
+
+public Map config
+
+def call( Map config) {
+  
+  print currentBuild.getBuildCauses().toString()
+  if (currentBuild.getBuildCauses().toString().contains('BranchIndexingCause')) {
+  print "INFO: Build skipped due to trigger being Branch Indexing"
+  currentBuild.result = 'ABORTED' // optves a better hint to the user tha's been skipped, rather than the default which shows it's successful
+  return
+  }
+  
+  this.configuration = configuration
+  
+  
+  
+}
