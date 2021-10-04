7716f15d6a235f3e1b7bded5e4fe8bed8ac877a8
sunkaravineeth <63944740+sunkaravineeth@users.noreply.github.com>
2021-10-04 18:33:20 +0530

    Update Deployment.groovy

--- vars/Deployment.groovy
+++ vars/Deployment.groovy
@@ -1,1 +1,61 @@
 
+public Map config
+
+def call( Map config) {
+  
+  print currentBuild.getBuildCauses().toString()
+  if (currentBuild.getBuildCauses().toString().contains('BranchIndexingCause')) {
+  print "INFO: Build skipped due to trigger being Branch Indexing"
+  currentBuild.result = 'ABORTED' // optves a better hint to the user tha's been skipped, rather than the default which shows it's successful
+  return
+  }
+  
+  this.config = config
+  
+  pipeline{
+    agent any
+    
+    options{
+      
+      timeout(
+        time: config.timeoutTime ? config.timeoutTime : 6,
+        unit: config.timeoutUnit ? config. timoutTUnit: "HOURS"
+      )
+      
+      disableConcurrentBuilds()
+      
+      buildDiscarder(
+        logRotator(
+          numToKeepStr: config.logRotatorNumToKeep ? config.logRotatorNumToKeep : '30', 
+          artifactNumToKeepStr: config.logRotatorArtifactNumToKeep ? config.logRotatorArtifactNumToKeep : '3'
+        )
+      )    
+    }
+    
+    environment{
+      def uploadSpec = """{
+     "files": [
+      {
+          "pattern": "**/*.jar",
+          "target": "game/"
+        }
+     ]
+     }"""
+    }
+    
+    stages{
+      stage("Initialization"){
+        steps{
+          script{
+            echo "---checking tools versions-------"
+            sh "java -version"
+            sh "mvn -v"
+            pom = readMavenPom file: "pom.xml"
+            echo "articat: ${pom.getArtifactId()}"
+            
+          }
+        }
+      }
+    }
+  }
+}
